fb038e816e0a4962d895b6c2420417ad52cdb8bb
nana-akwasi <nana505abgmail.com>
2022-05-14 09:25:35 -0500

    create stg and prd vpc

--- modules.tf
+++ modules.tf
@@ -3,3 +3,15 @@ module "vpc-dev" {
     cidr = "10.20.0.0/16"
     project_name = "dev-vpc"
 }
+
+module "vpc-stg" {
+    source = "./VPC"
+    cidr = "10.30.0.0/16"
+    project_name = "stg-vpc"
+}
+
+module "vpc-prd" {
+    source = "./VPC"
+    cidr = "10.40.0.0/16"
+    project_name = "prd-vpc"
+}
