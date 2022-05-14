af9eb1e54c3daa6212e71b73c70f6761145f4404
nana-akwasi <nana505abgmail.com>
2022-05-14 10:06:20 -0500

    create subnets

--- modules.tf
+++ modules.tf
@@ -39,21 +39,21 @@ module "igw-prd" {
 
 
 module "subn-dev" {
-    source = "./IGW"
+    source = "./SUBNETS"
     project_name = "dev-subn"
     vpc_id       = module.vpc-dev.id_vpc
     cidr         = "10.20.0.0/24"
 }
 
 module "subn-stg" {
-    source = "./IGW"
+    source = "./SUBNETS"
     project_name = "stg-subn"
     vpc_id       = module.vpc-stg.id_vpc
     cidr         = "10.30.0.0/24"
 }
 
 module "subn-prd" {
-    source = "./IGW"
+    source = "./SUBNETS"
     project_name = "prd-subn"
     vpc_id       = module.vpc-prd.id_vpc
     cidr         = "10.40.0.0/24"
